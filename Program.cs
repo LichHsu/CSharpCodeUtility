e7792b7b79d1792b55149df85ec6f940792ec3d5
LichHsu <anumis.lich@gmail.com>
2025-12-05 15:15:45 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpCodeUtility
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
